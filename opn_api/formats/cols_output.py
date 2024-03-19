c18c2df21f8cd0ff4ee505764c8796bfdfdc7f79
Devin Barry <devinbarry@users.noreply.github.com>
2024-03-19 17:50:25 +0000

    Remove unused code from opn-cli project.

--- opn_api/formats/cols_output.py
+++
@@ -1,7 +0,0 @@
-import click
-from opn_api.formats.base import Format
-
-
-class ColsOutputFormat(Format):
-    def echo(self):
-        click.echo(",".join(self._cols))
--- opn_api/formats/json_output.py
+++
@@ -1,9 +0,0 @@
-import click
-import json
-
-from opn_api.formats.base import Format
-
-
-class JsonOutputFormat(Format):
-    def echo(self):
-        click.echo(json.dumps(self._json_data))
