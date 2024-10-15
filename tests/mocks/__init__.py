c38cd3e608161958a2f17578e5e153d7cdeb99fb
Devin Barry <devinbarry@users.noreply.github.com>
2024-10-15 06:54:57 +0100

    Move mocks into alias package.

--- tests/mocks/__init__.py
+++
@@ -1,5 +0,0 @@
-from .mock_alias_export import mock_export_data
-from .mock_alias_get_detail import mock_get_detail_data
-from .mock_alias_get_item import mock_get_item_data
-from .mock_alias_search_item import mock_search_item_data
-from .mock_firewall_alias_data import *
---
+++ tests/mocks/alias/__init__.py
@@ -0,0 +1,5 @@
+from .mock_alias_export import mock_export_data
+from .mock_alias_get_detail import mock_get_detail_data
+from .mock_alias_get_item import mock_get_item_data
+from .mock_alias_search_item import mock_search_item_data
+from .mock_firewall_alias_data import *
